e728cb6f07c659147417858cf12f81973898ad4d
larrydaguilera <larrydaguilera@gmail.com>
2022-10-25 21:28:34 -0300

    Controller, service and models already done

--- Backend/src/main/java/hahn/software/challenge/backend/repository/ProductRepository.java
+++ Backend/src/main/java/hahn/software/challenge/backend/repository/ProductRepository.java
@@ -1,4 +1,11 @@
 package hahn.software.challenge.backend.repository;
 
-public class ProductRepository {
+import hahn.software.challenge.backend.models.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository <ProductEntity,Long> {
+
+
 }
--- Backend/src/main/java/hahn/software/challenge/backend/service/ProductService.java
+++ Backend/src/main/java/hahn/software/challenge/backend/service/ProductService.java
@@ -1,4 +1,16 @@
 package hahn.software.challenge.backend.service;
 
-public class ProductService {
+import hahn.software.challenge.backend.models.dto.ProductDto;
+
+import java.util.List;
+
+public interface ProductService {
+     List<ProductDto> getAll();
+     ProductDto getById(Long id);
+
+     ProductDto updateById(Long id);
+
+     ProductDto create(ProductDto dto);
+
+     void deleteById(Long id);
 }
